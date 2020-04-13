5469de0efff50d3e0fd1050ea5bf1bcb81f0cc85
Santiago Fraire Willemoës <santiwilly@gmail.com>
2020-04-13 16:28:50 +0200

    fix: text starts with uppercase

--- src/components/LogTable.js
+++ src/components/LogTable.js
@@ -16,31 +16,31 @@ const LogTable = (props) => {
           <tbody>
             <tr>
               <td className="font-bold text-debug">DEBUG</td>
-              <td>meaningful for developers, it, sysadmin</td>
+              <td>Meaningful for developers, IT, sysadmin</td>
               <td>-</td>
             </tr>
             <tr>
               <td className="font-bold text-info">INFO</td>
-              <td>useful information to have</td>
-              <td>start or stop service, configuration</td>
+              <td>Useful information to have</td>
+              <td>Start or stop service, configuration</td>
             </tr>
             <tr>
               <td className="font-bold text-warn">WARNING</td>
               <td>
-                can potentially cause application oddities, but will
+                Can potentially cause application oddities, but will
                 automatically recover
               </td>
               <td>
-                switching from a primary to backup server, retrying an operation
+                Switching from a primary to backup server, retrying an operation
               </td>
             </tr>
             <tr>
               <td className="font-bold text-error">ERROR</td>
               <td>
-                any error fatal to the operation, but not the service or
+                Any error fatal to the operation, but not the service or
                 application
               </td>
-              <td>can't open a required file, missing data</td>
+              <td>Can't open a required file, missing data</td>
             </tr>
           </tbody>
         </table>
